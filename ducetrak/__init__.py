bba71236e0c3523d590e3b887a22b41d92f18d83
Mukandila Mukandila <ndila@muka.io>
2020-09-05 11:29:23 -0400

    Moving towards app factory

--- ducetrak/__init__.py
+++ ducetrak/__init__.py
@@ -1,13 +1,21 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 
 
-def create_app()
+def create_app():
 
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    # db = SQLAlchemy(app)
+    # migrate = Migrate(app, db)
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
--- ducetrak/models.py
+++ ducetrak/models.py
@@ -1,13 +1,15 @@
-from ducetrak import db
+from sqlalchemy import Column, Integer, String
+from ducetrak.db import Base
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), index=True, unique=True)
-    email = db.Column(db.String(255), index=True, unique=True)
-    password = db.Column(db.String(128))
-    first_name = db.Column(db.String(255))
-    last_name = db.Column(db.String(255))
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True, unique=True)
+    email = Column(String(255), index=True, unique=True)
+    password = Column(String(128))
+    first_name = Column(String(255))
+    last_name = Column(String(255))
 
     def __repr__(self):
         return f'<User {self.username}>'
