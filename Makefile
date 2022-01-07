349e55afbd538505cab1861b40c36151e7493ea2
Mukandila Mukandila <ndila@muka.io>
2022-01-06 20:11:03 -0500

    Dev (#4) (#5)
    * Initial commit (#2)
    
    * Adding github action and stuff...
    
    * fixing linting errors
    
    * fixing linting in tests
    
    * Adding CD
    
    * staging
    
    * fixing (#3)
    
    * fixing
    
    * fixing stuff
    
    * fixing moar stuff
    
    * okok
    
    * more fixing
    
    * more ok
    
    * omfrfer
    
    * sfomsomfsdfroar

---
+++ Makefile
@@ -0,0 +1,16 @@
+SHELL=/bin/bash -o pipefail
+
+init:
+	pip3 install -r requirements.txt
+
+init-dev: init
+	pip3 install -r requirements-dev.txt
+
+lint:
+	flake8 app.py chalicelib/ tests/
+
+test:
+	pytest tests/
+
+deploy: init
+	chalice deploy --stage $(STAGE)
---
+++ app.py
@@ -0,0 +1,55 @@
+from chalicelib.handlers.produce_handler import ProduceHandler
+
+from chalice import Chalice, Response
+
+app = Chalice(app_name='ducetrak')
+
+
+@app.route('/')
+def index():
+    return {'ping': 'pong'}
+
+
+@app.route('/produce')
+def produce() -> Response:
+    return ProduceHandler().get_produce()
+
+
+@app.route('/type')
+def produce_type():
+    return {'produce': 'type'}
+
+
+@app.route('/price')
+def price():
+    return {'produce': 'price'}
+
+
+@app.route('/user')
+def user():
+    return {'user': 'user'}
+
+
+@app.route('/location')
+def location():
+    return {'location': 'location'}
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
